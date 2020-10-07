f55842973bc7f3d199a3ab62b6230a0c4bc882b5
LucasLaurens <lucaslaurens.74@gmail.com>
2020-10-07 14:30:38 +0200

    [init] Prepare templating

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const port = 8080
-
 let app = express()
 
-// Main Route
+// Set templating
+app.set('view engine', 'ejs')
+app.set('trust proxy', 1)
+
+// Main route
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.render('./../client/index.ejs', {
+		text_try: "salut les gens !!"
+	})
 })
 
 app.listen(port)
