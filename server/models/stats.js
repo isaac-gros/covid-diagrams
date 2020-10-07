02571a7441e64ec6411d59f35bf85676e74d7d5e
LucasLaurens <lucaslaurens.74@gmail.com>
2020-10-07 16:12:53 +0200

    [Back] create a new item with dynamoDB

--- server/models/stats.js
+++ server/models/stats.js
@@ -1,7 +1,7 @@
 let AWS = require('../../config/config')
 
 const docClient = new AWS.DynamoDB.DocumentClient();
-const params = {
+let params = {
 	TableName: 'Stats'
 };
 
@@ -9,7 +9,23 @@ const params = {
 
 class Stats {
 	/**
-     * Get all stats
+	 * Create new item
+	 */
+    static create (object, callback) {
+		params.Item = object
+
+		docClient.put(params, function(err, data) {
+			if (err) {
+				console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+			} else {
+				callback(JSON.stringify(data, null, 2));
+			}
+		});
+    }
+
+
+	/**
+     * Get all items
      */
     static all (callback) {
         docClient.scan(params, function (err, data) {
--- server/server.js
+++ server/server.js
@@ -1,20 +1,60 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid');
 
 const port = 8080
 let app = express()
+let bodyParser = require('body-parser')
 let Stats = require('./models/stats')
 
 // Set templating
 app.set('view engine', 'ejs')
 app.set('trust proxy', 1)
 
-// Main route
+// Set Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+/**
+ * Get all stats
+ */
 app.get('/', (req, res) => {
 	Stats.all( function (stats) {
+		console.log(stats)
         res.render('./../client/base.ejs', {
 			stats: stats
 		})
     })
+})
+
+/**
+ * Create a new stat item
+ */
+app.post('/', (req, res) => {
+	// Create the request object
+	let object = {
+		stat_id: uuidv4(),
+		death_count: req.body.death_count,
+		heal_count: req.body.heal_count,
+		new_hospitalize_count: req.body.new_hospitalize_count,
+		source: req.body.source,
+		state_name: req.body.state_name,
+		hospitalize_count: req.body.hospitalize_count,
+		new_intensive_care_count: req.body.new_intensive_care_count,
+		created_at: new Date().getTime(),
+		updated_at: new Date().getTime(),
+		state_code: req.body.state_code,
+		date: req.body.date,
+		intensive_care_count: req.body.intensive_care_count
+	}
+
+	try {
+		Stats.create(object, function () {
+			res.redirect('/')
+		})
+	} catch (e) {
+		console.error(e)
+		res.redirect('/')
+	}
 
 })
 
