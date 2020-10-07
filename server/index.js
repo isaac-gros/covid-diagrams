a0374d4adb4dbc7ad4025c71b1658b9c66cd9f0a
Isaac Gros <isaac.gros888@gmail.com>
2020-10-07 16:11:49 +0200

    [FEAT] Static front page

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const port = 8080
 let app = express()
 
+// Load static styles
+app.use('/assets', express.static('public'))
+app.use('/node_modules', express.static('node_modules'))
+
 // Set templating
 app.set('view engine', 'ejs')
 app.set('trust proxy', 1)
